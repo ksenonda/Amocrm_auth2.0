579d8f8ddf78c524f2b0ab57027773752bc29178
ksenonda <ksenonda@users.noreply.github.com>
2021-01-15 11:15:13 +0300

    Update CatalogElement.php

--- src/Models/CatalogElement.php
+++ src/Models/CatalogElement.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace AmoCRM\Models;
+namespace AmoCRM2\Models;
 
 /**
  * Class CatalogElement
  *
  * Класс модель для работы с Элементами каталога
  *
- * @package AmoCRM\Models
+ * @package AmoCRM2\Models
  * @author mb@baso-it.ru
  * @author dotzero <mail@dotzero.ru>
  * @link https://github.com/dotzero/amocrm-php
