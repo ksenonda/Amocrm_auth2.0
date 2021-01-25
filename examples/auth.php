82c6a2b20e5ff783a4b59309789e73389b38781d
ksenonda <ksenonda@users.noreply.github.com>
2021-01-25 15:07:42 +0300

    Update auth.php

--- examples/auth.php
+++ examples/auth.php
@@ -1,40 +1,42 @@
 <?php
-use AmoCRM\OAuth2\Client\Provider\AmoAuth;
+// авторизация по auth 2.0
 
-include_once '/var/www/vexor/vendor/autoload.php';
+use AmoCRM\OAuth2\Client\Provider\AmoCRM;
+include_once '.../vendor/autoload.php';
 include_once __DIR__ . '/AmoCRM.php';
+// связь с бд
+require_once __DIR__.'/lib/bdconfig.php';
+require_once __DIR__.'/lib/classes.php';
+
+$sql = "SELECT * FROM ... WHERE ...";
+$result = $pdo->getData($sql);
+$accessToken = $result[0];
 
 //задаем данные клиента
-$provider = new AmoAuth([
-    'clientId' => '',
-    'clientSecret' => '',
-    'redirectUri' => '',
+$provider = new AmoCRM([
+    'clientId' => '...',
+    'clientSecret' => '...',
+    'redirectUri' => '...',
 ]);
-//проверка токена
-$accessToken = $provider->getToken('token_file.txt');
-$provider->setBaseDomain($accessToken->getValues()['baseDomain']);
-if ($accessToken->hasExpired()) {
-    try {
-        $accessToken = $provider->getAccessToken(new League\OAuth2\Client\Grant\RefreshToken(), [
-            'refresh_token' => $accessToken->getRefreshToken(),
-        ]);
-        $provider->saveToken([
-            'accessToken' => $accessToken->getToken(),
-            'refreshToken' => $accessToken->getRefreshToken(),
-            'expires' => $accessToken->getExpires(),
-            'baseDomain' => $provider->getBaseDomain(),
-        ], 'token_file.txt');
-    } catch (Exception $e) {
-        die((string)$e);
-    }
-}
 
-//получение токена для дальнейших запросов
-$token = $accessToken->getToken('token_file.txt');
-//формируем поля заголовков для запросов
-$headers = [
-    'Content-Type:application/json',
-    'Authorization: Bearer ' . $token,
-];
-//ссылка к кабинету клиента
-$full_domain = $provider->urlAccount(); 
+$accessToken = $provider->getToken($accessToken);
+$provider->setBaseDomain($accessToken->getValues()['baseDomain']);
+if ($accessToken->hasExpired()) 
+        {
+                $accessToken = $provider->getAccessToken(new League\OAuth2\Client\Grant\RefreshToken(), [
+                'refresh_token' => $accessToken->getRefreshToken(),
+                ]);
+
+                $token = $accessToken->getToken();
+                $refresh_token = $accessToken->getRefreshToken();
+                $expires = $accessToken->getExpires();
+                $base_domain = $provider->getBaseDomain();
+
+                $sql = "UPDATE ... SET ... WHERE ...";
+                $pdo->query($sql);
+                
+        }
+$token = $accessToken->getToken();
+$account_link = $provider->urlAccount();
+
+$amo = new \AmoCRM2\Client($account_link, $token); 
