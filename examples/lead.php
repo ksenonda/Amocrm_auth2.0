02dce917d9defd837c2814d918f55b1c6b16c843
ksenonda <ksenonda@users.noreply.github.com>
2021-01-25 15:08:17 +0300

    Update lead.php

--- examples/lead.php
+++ examples/lead.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
-    $amo = new \AmoCRM\Client(getenv('DOMAIN'), getenv('LOGIN'), getenv('HASH'));
+    $amo = new \AmoCRM2\Client($account_link, $token);
 
     // Список сделок
     // Метод для получения списка сделок с возможностью фильтрации и постраничной выборки.
