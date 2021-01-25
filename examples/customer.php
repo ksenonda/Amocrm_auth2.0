67c7448f20ad99678dc4db2fd88eac33c25aee67
ksenonda <ksenonda@users.noreply.github.com>
2021-01-25 15:08:00 +0300

    Update customer.php

--- examples/customer.php
+++ examples/customer.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
-    $amo = new \AmoCRM\Client(getenv('DOMAIN'), getenv('LOGIN'), getenv('HASH'));
+    $amo = new \AmoCRM2\Client($account_link, $token);
 
     // Список покупателей
     // Метод для получения покупателей аккаунта
