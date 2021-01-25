6422102c2b11b62b9452d2912da87e90f3d7cce0
ksenonda <ksenonda@users.noreply.github.com>
2021-01-25 15:07:40 +0300

    Update account.php

--- examples/account.php
+++ examples/account.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
-    $amo = new \AmoCRM\Client(getenv('DOMAIN'), getenv('LOGIN'), getenv('HASH'));
+    $amo = new \AmoCRM2\Client($account_link, $token);
 
     // Получение информации по аккаунту в котором произведена авторизация:
     // название, оплаченный период, пользователи аккаунта и их права,
