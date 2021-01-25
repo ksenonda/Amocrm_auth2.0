f70a6481060e5b2c2d2fb7fd10c318230099557d
ksenonda <ksenonda@users.noreply.github.com>
2021-01-25 15:07:52 +0300

    Update company.php

--- examples/company.php
+++ examples/company.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
-    $amo = new \AmoCRM\Client(getenv('DOMAIN'), getenv('LOGIN'), getenv('HASH'));
+    $amo = new \AmoCRM2\Client($account_link, $token); 
 
     // Список компаний
     // Метод для получения списка компаний с возможностью фильтрации и постраничной выборки.
