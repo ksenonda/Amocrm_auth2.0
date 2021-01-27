ed367cd999010abb40dd9aead53aa72a5e6e3a42
ksenonda <ksenonda@users.noreply.github.com>
2021-01-27 12:11:56 +0300

    Update summary.php

--- examples/unsorted/summary.php
+++ examples/unsorted/summary.php
@@ -3,13 +3,13 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 try {
-    $amo = new \AmoCRM\Client(getenv('DOMAIN'), getenv('LOGIN'), getenv('HASH'));
+    $amo = new \AmoCRM2\Client($account_link, $token);
 
     // Агрегирование неразобранных заявок
     // Метод для получения агрегированной информации о неразобранных заявках.
 
     print_r($amo->unsorted->apiGetAllSummary());
 
-} catch (\AmoCRM\Exception $e) {
+} catch (\AmoCRM2\Exception $e) {
     printf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage());
 }
