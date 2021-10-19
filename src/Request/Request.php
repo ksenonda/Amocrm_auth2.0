109f472c166c9f070eb30db733c241612567ebdd
ksenonda <lpkoifish@gmail.com>
2021-10-19 14:05:12 +0300

    Update Request.php

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -138,6 +138,45 @@ class Request
         return $this->request($url);
     }
 
+    protected function postv4Request($url, $parameters = [])
+    {
+        $headers = $this->prepareHeaders($modified);
+        $endpoint = $this->prepareEndpoint($url);
+
+        $ch = $this->curlHandle->open();
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'amoCRM-oAuth-client/1.0');
+        curl_setopt($ch, CURLOPT_URL, $endpoint);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        if (!empty($parameters)) 
+        {
+          curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($parameters));
+        }
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+
+        $result = curl_exec($ch);
+        $info = curl_getinfo($ch);
+        $error = curl_error($ch);
+        $errno = curl_errno($ch);
+
+        $this->curlHandle->close();
+
+        $this->lastHttpCode = $info['http_code'];
+        $this->lastHttpResponse = $result;
+
+
+        if ($result === false && !empty($error)) 
+        {
+            throw new NetworkException($error, $errno);
+        }
+
+        return $this->parseResponse($result, $info);
+    }
+
     protected function patchRequest($url, $parameters = [], $modified = null)
     {
         $headers = $this->prepareHeaders($modified);
