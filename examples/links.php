fd124792b46b032d0297bc5e3909b0422529e643
ksenonda <lpkoifish@gmail.com>
2021-11-08 10:49:40 +0300

    Update links.php

--- examples/links.php
+++ examples/links.php
@@ -25,6 +25,30 @@ try {
     $link['to_id'] = 3673249;
     var_dump($link->apiLink());
 
+    // v4 - пример привязки элементов каталога
+    $link = $amo->links;
+    $link->debug(true); // Режим отладки
+    $link['from'] = 'leads';
+    $link['from_id'] = 12012159;
+    $link['to'] = 'catalog_elements';
+    $link['to_id'] = 393793;
+    $link['catalog_id'] = 4849;
+    $link['quantity'] = 1;
+    //$link['price_id'] = 1234;
+    //$link['main_contact'] = true;
+    $res = $link->apiv4Link();
+
+    // v4 - пример отвязки элементов каталога
+    $link = $amo->links;
+    $link->debug(true); // Режим отладки
+    $link['from'] = 'leads';
+    $link['from_id'] = 12012159;
+    $link['to'] = 'catalog_elements';
+    $link['to_id'] = 393793;
+    $link['catalog_id'] = 4849;
+    //$link['updated_by'] = 0;
+    $res = $link->apiv4Unink();
+
     // Разрыв связи между сущностями
     // Метод позволяет удалять связи между сущностями
 
