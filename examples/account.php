16a7ab73b6fb2888d57d2224e033a67b17b6099b
ksenonda <lpkoifish@gmail.com>
2022-04-14 16:26:01 +0300

    Update account.php

--- examples/account.php
+++ examples/account.php
@@ -2,7 +2,8 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-try {
+try 
+{
     $amo = new \AmoCRM2\Client($account_link, $token);
 
     // Получение информации по аккаунту в котором произведена авторизация:
@@ -19,6 +20,23 @@ try {
     // Возвращает сведения об авторизвавшемся пользователе.
     print_r($amo->account->apiUser());
 
-} catch (\AmoCRM2\Exception $e) {
+    //
+    // Примеры V4 
+    //
+    
+    //Получение всех пользователей
+    //параметры не обязательные к заполнению
+    $users = $amo->account->apiv4Users([
+        'with' => ['role', 'group', 'uuid', 'amojo_id'], // доп параметры
+        'limit' => 250, // кол-во шт на странице
+        'page' => 1, // номер страницы]);
+    ]);
+
+    // Получение 1 пользователя по id
+    $user = $amo->account->apiv4User([000000, ['with' => []]]);  
+
+} 
+catch (\AmoCRM2\Exception $e) 
+{
     printf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage());
 }
