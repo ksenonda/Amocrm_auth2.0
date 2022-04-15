aef76e95c5946804c8c0eeeb8e6cc6f0d7644dc3
ksenonda <lpkoifish@gmail.com>
2022-04-15 12:16:19 +0300

    Update Company.php

--- src/Models/Company.php
+++ src/Models/Company.php
@@ -30,15 +30,23 @@ class Company extends AbstractModel
     protected $fields = [
         'id',
         'name',
-        'request_id',
-        'date_create',
-        'last_modified',
         'responsible_user_id',
+        'created_by',
         'created_user_id',
-        'linked_leads_id',
-        'tags',
+        'updated_by',
         'modified_user_id',
-        'custom_fields_values'
+        'created_at',
+        'date_create',
+        'updated_at',
+        'closest_task_at',
+        'last_modified',
+        'custom_fields_values',
+        'is_deleted',
+        'linked_companies_id',
+        'tags',
+        'request_id',
+        'account_id',
+        '_embedded'
     ];
 
     /**
@@ -57,6 +65,42 @@ class Company extends AbstractModel
         $response = $this->getRequest('/private/api/v2/json/company/list', $parameters, $modified);
 
         return isset($response['contacts']) ? $response['contacts'] : [];
+    }
+
+    /**
+     * Список компаний, метод в4
+     *
+     * Метод для получения списка компаний с возможностью фильтрации и постраничной выборки.
+     * Ограничение по возвращаемым на одной странице (offset) данным - 250 компаний.
+     *
+     * @link https://www.amocrm.ru/developers/content/crm_platform/companies-api#companies-list
+     * @param array $parameters Массив параметров к amoCRM API
+     * @param null|string $modified Дополнительная фильтрация по (изменено с)
+     * @return array Ответ amoCRM API
+     */
+    public function apiv4List($parameters, $modified = null)
+    {
+        $response = $this->getRequest('/api/v4/companies', $parameters, $modified);
+
+        return isset($response['_embedded']['companies']) ? $response['_embedded']['companies'] : [];
+    }
+
+    /**
+     * Получение компании по id, метод в4
+     *
+     * Метод позволяет получить данные конкретной компании по ID
+     * 
+     *
+     * @link https://www.amocrm.ru/developers/content/crm_platform/companies-api#company-detail
+     * @param array $parameters Массив параметров к amoCRM API
+     * @param null|string $modified Дополнительная фильтрация по (изменено с)
+     * @return array Ответ amoCRM API
+     */
+    public function apiv4One($id, $parameters = [])
+    {
+        $response = $this->getRequest('/api/v4/companies/'.$id, $parameters);
+
+        return isset($response) ? $response : [];;
     }
 
     /**
@@ -98,6 +142,39 @@ class Company extends AbstractModel
     }
 
     /**
+     * Добавление компании, метод в4
+     *
+     * Метод позволяет добавлять компании по одной или пакетно
+     *
+     * @link https://www.amocrm.ru/developers/content/crm_platform/companies-api#companies-add
+     * @param array $companies Массив компаний для пакетного добавления
+     * @return array Массив данных по компании(компаниям)
+     */
+    public function apiv4Add($companies = [])
+    {
+        if (empty($companies)) 
+        {
+            $companies = [$this];
+        }
+
+        $parameters = [];
+
+        foreach ($companies as $company) 
+        {
+            $values = $company->getValues();
+            if (isset($values['tags']))
+            {
+                $values['_embedded']['tags'] = $this->handleTags($values['tags']);
+            }
+            $parameters[] = $values;    
+        }
+
+        $response = $this->postv4Request('/api/v4/companies', $parameters);
+
+        return isset($response['_embedded']['companies']) ? $response['_embedded']['companies'] : [];
+    }
+
+    /**
      * Обновление компаний
      *
      * Метод позволяет обновлять данные по уже существующим компаниям
@@ -128,9 +205,22 @@ class Company extends AbstractModel
 
         return empty($response['companies']['update']['errors']);
     }
-
-    public function apiv4Update(array $companies, $modified = 'now')
+    /**
+     * Обновление компаний, метод в4
+     *
+     * Метод позволяет обновлять данные по уже существующим компаниям
+     *
+     * @link https://www.amocrm.ru/developers/content/crm_platform/companies-api#companies-edit
+     * @param string $modified Дата последнего изменения данной сущности
+     * @throws \AmoCRM\Exception
+     */
+    public function apiv4Update($companies = [], $modified = 'now')
     {
+        if (empty($companies))
+        {
+            $companies = [$this];
+        }
+
         $parameters = [];
 
         foreach ($companies AS $company) 
@@ -145,8 +235,12 @@ class Company extends AbstractModel
             {
                 $updated_values['custom_fields_values'] = $this->handleCustomFields($updated_values['custom_fields_values']);
             }
+            if (isset($updated_values['tags']))
+            {
+                $updated_values['_embedded']['tags'] = $this->handleTags($updated_values['tags']);
+            }
 
-            $updated_values['last_modified'] = strtotime($modified);
+            $updated_values['updated_at'] = strtotime($modified);
             $parameters[] = $updated_values; 
         }
 
