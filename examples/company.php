b1191daa2e84c50bf71aefe0d141758c8069f0fd
ksenonda <lpkoifish@gmail.com>
2022-04-15 17:54:33 +0300

    Update company.php

--- examples/company.php
+++ examples/company.php
@@ -2,7 +2,70 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-try {
+/**
+*
+* Примеры версии в4
+*
+*/
+
+try 
+{
+    $amo = new \AmoCRM2\Client($account_link, $token);
+
+    // Список компаний
+    $companies = $amo->company->apiv4List(
+    [
+        //список всех возможных параметров ф-ии
+        'id' => 000000, // поиск по id компании
+        'query' => '8005553535', // поисковый запрос
+        'with' => ['catalog_elements', 'leads', 'contacts', 'customers'], // доп параметры
+        'limit' => 250, // кол-во шт на странице
+        'page' => 1, // номер страницы
+        'order[updated_at|id]' => 'asc|desc', //сортировка доступна по 2 типам полей updated_at или id и по 2 значениям asc или desc
+        'filter[...]' => '...'|[], // https://www.amocrm.ru/developers/content/crm_platform/filters-api по фильтрации
+    ]);
+
+    //Контакт по id, доступны доп параметры через with (необязательно)
+    $company = $amo->company->apiv4One(000000, ['with' => []]);
+
+    // Добавление компании
+    $company = $amo->company;
+    $company['name'] = 'Тестовая компания через в4';
+    $company->addv4CustomField(167411, 'Спецпроект'); // добавляет кастомное поле
+    $company->addTags(['тест1', 'тест2']); //добавляет тэги
+    $result = $company->apiv4Add();
+
+    // Обновление компании
+    $company = $amo->company;
+    $company['id'] = 24180715;
+    $company['name'] = 'Тестовая компания: новое название';
+    $result = $company->apiv4Update();
+
+    // Или массовое обновление:
+    $company = $amo->company;
+    $company->debug(true); // Режим отладки
+    $company1 = clone $company;
+    $company1['id'] = 24180715;
+    $company1['name'] = 'Тестовая компания 1: новое название';
+    $company2 = clone $company;
+    $company2['id'] = 24190891;
+    $company2['name'] = 'Тестовая компания 2: новое название';
+
+    $result = $amo->company->apiv4Update([$company1, $company2]);
+
+} 
+catch (\AmoCRM2\Exception $e) 
+{
+    printf('Error (%d): %s', $e->getCode(), $e->getMessage());
+}
+
+/**
+*
+* Примеры версии в2
+*
+*/
+try 
+{
     $amo = new \AmoCRM2\Client($account_link, $token); 
 
     // Список компаний
@@ -86,6 +149,8 @@ try {
 
     $result = $amo->company->apiv4Update([$company1, $company2]);
 
-} catch (\AmoCRM2\Exception $e) {
+} 
+catch (\AmoCRM2\Exception $e) 
+{
     printf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage());
 }
