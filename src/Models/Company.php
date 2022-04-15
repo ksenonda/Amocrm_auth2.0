8bf332179adac85b4f6993ad8fc8273ce79b5e16
ksenonda <lpkoifish@gmail.com>
2022-04-15 15:56:41 +0300

    Update Company.php

--- src/Models/Company.php
+++ src/Models/Company.php
@@ -162,6 +162,10 @@ class Company extends AbstractModel
         foreach ($companies as $company) 
         {
             $values = $company->getValues();
+            if (isset($values['custom_fields_values']))
+            {
+                $values['custom_fields_values'] = $this->handleCustomFields($values['custom_fields_values']);
+            }
             if (isset($values['tags']))
             {
                 $values['_embedded']['tags'] = $this->handleTags($values['tags']);
