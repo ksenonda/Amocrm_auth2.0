f8514398882a9ee277b784af7e124557983f6c3c
ksenonda <lpkoifish@gmail.com>
2022-04-20 10:21:12 +0300

    Update Webhooks.php

--- src/Models/Webhooks.php
+++ src/Models/Webhooks.php
@@ -23,6 +23,7 @@ class Webhooks extends AbstractModel
     protected $fields = [
         'url',
         'events',
+        'filter'
     ];
 
     /**
@@ -33,16 +34,25 @@ class Webhooks extends AbstractModel
         'add_contact', // Добавить контакт
         'add_company', // Добавить компанию
         'add_customer', // Добавить покупателя
+        'add_talk', //Добавлена беседа
+        'add_task', //Добавлена задача
         'update_lead', // Изменить сделку
         'update_contact', // Изменить контакт
         'update_company', // Изменить компанию
         'update_customer', // Изменить покупателя
+        'update_talk', //Беседа изменена
+        'update_task', //Задача изменена
         'delete_lead', // Удалить сделку
         'delete_contact', // Удалить контакт
         'delete_company', // Удалить компанию
         'delete_customer', // Удалить покупателя
+        'delete_task', //Удалена задача
         'status_lead', // Смена статуса сделки
         'responsible_lead', // Смена отв-го сделки
+        'responsible_contact', //У контакта сменился ответственный
+        'responsible_company', //У компании сменился ответственный
+        'responsible_customer', //У покупателя сменился ответственный
+        'responsible_task', //У задачи сменился ответственный
         'restore_contact', // Восстановить контакт
         'restore_company', // Восстановить компанию
         'restore_lead', // Восстановить сделку
@@ -87,6 +97,21 @@ class Webhooks extends AbstractModel
     }
 
     /**
+     * Список установленных вебхуков в аккаунте, метод в4
+     *
+     * Метод для получения списка Webhooks.
+     *
+     * @link https://www.amocrm.ru/developers/content/crm_platform/webhooks-api#webhooks-list
+     * @return array Ответ amoCRM API
+     */
+    public function apiv4List($parameters = [])
+    {
+        $response = $this->getRequest('/api/v4/webhooks', $parameters);
+
+        return isset($response['_embedded']['webhooks']) ? $response['_embedded']['webhooks'] : [];
+    }
+
+    /**
      * Добавление Webhooks
      *
      * Метод для добавления Webhooks.
@@ -128,6 +153,29 @@ class Webhooks extends AbstractModel
     }
 
     /**
+     * Подписка на вебхук, метод в4
+     *
+     * Метод позволяет подписываться на события, информация о которым придет на указанный адрес.
+     *
+     * @link https://www.amocrm.ru/developers/content/crm_platform/webhooks-api#webhook-subscribe
+     * @param string $url URL на который необходимо присылать уведомления, должен соответствовать стандарту RFC 2396
+     * @param array $events Список событий, при которых должны отправляться Webhooks
+     * @return array Ответ amoCRM API
+     * @throws \AmoCRM\Exception
+     */
+    public function apiv4Subscribe($url, array $settings)
+    {
+        $parameters = [
+            'destination' => $url,
+            'settings' => $settings,
+        ];
+
+        $response = $this->postv4Request('/api/v4/webhooks', $parameters);
+
+        return isset($response) ? $response : [];
+    }
+
+    /**
      * Удаления Webhooks
      *
      * Метод для удаления Webhooks.
@@ -166,5 +214,23 @@ class Webhooks extends AbstractModel
         }
 
         return false;
+    }
+
+    /**
+     * Отписка от событий, метод в4
+     *
+     * Метод позволяет отписать вебхук от получения любых событий
+     *
+     * @link https://www.amocrm.ru/developers/content/crm_platform/webhooks-api#webhooks-delete
+     * @param string $url URL на который необходимо присылать уведомления, должен соответствовать стандарту RFC 2396
+     * @param array $events Список событий, при которых должны отправляться Webhooks
+     * @return array Ответ amoCRM API
+     * @throws \AmoCRM\Exception
+     */
+    public function apiv4Unsubscribe()
+    {
+        $response = $this->deletev4Request('/api/v4/webhooks', $parameters);
+
+        return isset($response) ? $response : [];
     }
 }
