90e38938dff2b46edb95a29003d3530f85c218af
ksenonda <lpkoifish@gmail.com>
2022-04-25 11:34:11 +0300

    Update webhooks.php

--- examples/webhooks.php
+++ examples/webhooks.php
@@ -1,8 +1,43 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+/**
+*
+* Примеры версии в4
+*
+*/
 
-try {
+try 
+{
+    $amo = new \AmoCRM2\Client($account_link, $token); 
+
+    // Список Webhooks
+    // Метод для получения списка Webhooks.
+
+    $webhooks = $amo->webhooks->apiv4List();
+    // доступна фильтрация по url вебхука
+    $webhooks = $amo->webhooks->apiv4List(['filter[destination]' => 'https://ingeni.app/0/support/telegram/']);
+
+    // подписка на события
+    $result = $amo->webhooks->apiv4Subscribe('https://ingeni.app/modules/webhook/', ['responsible_lead']);
+
+    //отписка от всех событий
+    $result = $amo->webhooks->apiv4Unsubscribe('https://ingeni.app/modules/webhook/');
+
+} 
+catch (\AmoCRM2\Exception $e) 
+{
+    printf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage());
+}
+
+
+/**
+*
+* Примеры версии в2
+*
+*/
+try 
+{
     $amo = new \AmoCRM2\Client($account_link, $token); 
 
     // Список Webhooks
@@ -38,6 +73,8 @@ try {
         'delete_contact'
     ]));
 
-} catch (\AmoCRM2\Exception $e) {
+} 
+catch (\AmoCRM2\Exception $e) 
+{
     printf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage());
 }
