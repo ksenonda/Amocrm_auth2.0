aab528478ee66631469ddfe42cf863f8a72443f4
ksenonda <lpkoifish@gmail.com>
2022-04-25 11:34:02 +0300

    Update call.php

--- examples/call.php
+++ examples/call.php
@@ -2,25 +2,34 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-try {
+try 
+{
     $amo = new \AmoCRM2\Client($account_link, $token); 
 
-    // Добавление звонков
-    // Метод позволяет добавлять звонки по одному или пакетно
+    // Добавление звонков, метод в4
+    // Данный метод позволяет пакетно добавлять звонки в карточки сущностей
 
     $call = $amo->call;
     $call->debug(true); // Режим отладки
-    $call['account_id'] = 1111111;
-    $call['uuid'] = '947669bc-ec58-450e-83e8-828a3e6fc354';
-    $call['caller'] = '88001000000';
-    $call['to'] = '88002000000';
-    $call['date'] = 'now';
-    $call['type'] = \AmoCRM\Models\Call::TYPE_INBOUND;
-    $call['billsec'] = 60;
-    $call['link'] = 'http://example.com/audio.mp3';
+    // обязательные поля
+    $call['phone'] = '78005553535'; // поиск производится по последним 10 цифрам номера
+    $call['direction'] = 'inbound'|'outbound'; // входящий или исходящий
+    $call['duration'] = 120; // длительность в сек
+    $call['source'] = 'sipuni'; // источник
+    // необязательные поля
+    $call['link'] = 'http://example.com/audio.mp3'; // запись
+    $call['uuid'] = '947669bc-ec58-450e-83e8-828a3e6fc354'; // id звонка
+    $call['call_result'] = 'Успешный разговор'; // результат звонка, типа комментарий
+    $call['call_status'] = 4; //Статус звонка. Доступные варианты: 1 – оставил сообщение, 2 – перезвонить позже, 3 – нет на месте, 4 – разговор состоялся, 5 – неверный номер, 6 – Не дозвонился, 7 – номер занят. 
+    $call['responsible_user_id'] = 0; // кому звонок
+    $call['created_by'] = 0;
+    $call['updated_by'] = 0;
+    $call['created_at'] = 1650870500;
+    $call['updated_at'] = 1650870500;
+    $result = $call->apiv4Add();
 
-    $call->apiAdd('my_service_name', '601eb8fab9707d8009dba552f2d411a3');
-
-} catch (\AmoCRM2\Exception $e) {
+} 
+catch (\AmoCRM2\Exception $e) 
+{
     printf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage());
 }
