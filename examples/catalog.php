0ce1727a3d45c12add452acb4b97195159bd7deb
ksenonda <lpkoifish@gmail.com>
2022-04-25 11:46:15 +0300

    Update catalog.php

--- examples/catalog.php
+++ examples/catalog.php
@@ -1,8 +1,53 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+/**
+*
+* Примеры версии в4
+*
+*/
 
-try {
+try 
+{
+    $amo = new \AmoCRM2\Client($account_link, $token); 
+
+    // Список каталогов
+    // Метод для получения списка каталогов аккаунта.
+    $catalogs = $amo->catalog->apiv4List();
+    $catalogs = $amo->catalog->apiv4List(['page' => 1, 'limit' => 250]);
+
+    // получение списка по id 
+    $result = $amo->catalog->apiv4One(7163);
+
+    //добавление списка
+    $catalog = $amo->catalog;
+    $catalog['name'] = 'Тестовый список'; // Обязательный параметр
+    $catalog['type'] = 'regular';
+    $catalog['can_add_elements'] = true;
+    $catalog['can_link_multiple'] = true;
+    $result = $catalog->apiv4Add();
+
+    //изменение списка
+    $catalog = $amo->catalog;
+    $catalog['name'] = 'Тестовый список: новое название'; // Обязательный параметр - даже если не меняется
+    $catalog['id'] = 8671;
+    $catalog['can_add_elements'] = false;
+    $catalog['can_link_multiple'] = false;
+    $result = $catalog->apiv4Update();
+
+} 
+catch (\AmoCRM2\Exception $e) 
+{
+    printf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage());
+}
+
+/**
+*
+* Примеры версии в2
+*
+*/
+try 
+{
     $amo = new \AmoCRM2\Client($account_link, $token); 
 
     // Список каталогов
@@ -43,6 +88,8 @@ try {
     // Удаление каталогов
     $amo->catalog->apiDelete((int)$id);
 
-} catch (\AmoCRM2\Exception $e) {
+} 
+catch (\AmoCRM2\Exception $e) 
+{
     printf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage());
 }
